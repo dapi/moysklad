989c014e94368c2c6cccf676ac4daaf33b2373f9
Danil Pismenny <danil@orionet.ru>
2014-11-27 12:36:18 +0400

    XmlFix

--- lib/moysklad/entities/xml_fix.rb
+++ lib/moysklad/entities/xml_fix.rb
@@ -3,17 +3,12 @@ module Moysklad::Entities::XmlFix
 
   included do
 
-    def to_utf_xml
-      #to_xml(Nokogiri::XML::Builder.new(encoding: 'utf-8')).to_xml
-      to_xml
-    end
-
     def to_xml
       super(Nokogiri::XML::Builder.new(encoding: 'utf-8')).to_xml
     end
 
     def to_s
-      to_utf_xml.to_s
+      to_xml.to_s
     end
   end
 end

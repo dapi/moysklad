e03e422438fa3c4fb4d230d1960e4caea0fa46f9
Danil Pismenny <danil@orionet.ru>
2014-11-28 18:20:36 +0400

    refactor XmlFix

--- lib/moysklad/entities/xml_fix.rb
+++ lib/moysklad/entities/xml_fix.rb
@@ -1,14 +1,12 @@
 module Moysklad::Entities::XmlFix
   extend ::ActiveSupport::Concern
 
-  included do
+  def to_xml *args
+    nokogiri = Nokogiri::XML::Builder.new(encoding: 'utf-8')
+    super(nokogiri).to_xml
+  end
 
-    def to_xml
-      super(Nokogiri::XML::Builder.new(encoding: 'utf-8')).to_xml
-    end
-
-    def to_s
-      to_xml.to_s
-    end
+  def to_s
+    to_xml.to_s
   end
 end

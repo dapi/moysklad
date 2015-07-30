474f8511ed74ce9ed2247f13af214d1f931ce38f
Danil Pismenny <danil@brandymint.ru>
2015-07-30 11:11:26 +0300

    Именованные ошибки

--- lib/moysklad/resources/indexed.rb
+++ lib/moysklad/resources/indexed.rb
@@ -2,10 +2,13 @@ require_relative 'where_filter'
 
 module Moysklad::Resources
   class Indexed  < SimpleDelegator
+    WrongEntriesCountError = Class.new StandardError
+    ObjectHaveNoUuidError  = Class.new StandardError
+
     include WhereFilter
 
     def initialize resource
-      raise 'resource должен быть Moysklad::Resources::Base' unless resource.is_a? Moysklad::Resources::Base
+      raise TypeError, 'resource должен быть Moysklad::Resources::Base' unless resource.is_a? Moysklad::Resources::Base
       super resource
     end
 
@@ -49,7 +52,7 @@ module Moysklad::Resources
       @_index
     end
 
-    def pull_list 
+    def pull_list
       @cached_list = load_full_list
       @_index = prepare_index @cached_list
       @cached_list
@@ -68,7 +71,7 @@ module Moysklad::Resources
         start = list.count
       end while start<_page.total
 
-      raise "При загрузке коллекции в результате колиество не совпадает с total: #{list.count}<>#{_page.total}" unless list.count==_page.total
+      raise WrongEntriesCountError, "При загрузке коллекции в результате колиество не совпадает с total: #{list.count}<>#{_page.total}" unless list.count==_page.total
 
       list
     end
@@ -76,7 +79,7 @@ module Moysklad::Resources
     def prepare_index cached_list
       i={}
       cached_list.each do |r|
-        raise "У объекта нет uuid: #{r.to_xml}" unless r.respond_to?(:uuid) && r.uuid
+        raise ObjectHaveNoUuidError, "У объекта нет uuid: #{r.to_xml}" unless r.respond_to?(:uuid) && r.uuid
         i[r.uuid]=r
       end
       return i
